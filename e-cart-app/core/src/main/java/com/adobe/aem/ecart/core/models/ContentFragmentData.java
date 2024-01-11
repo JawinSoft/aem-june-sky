5f9d4ca01bdfd83f82b623b319d306cb36f6f6ca
JawinSoft <41267120+JawinSoft@users.noreply.github.com>
2024-01-10 22:51:31 -0800

    Update ContentFragmentData.java

--- e-cart-app/core/src/main/java/com/adobe/aem/ecart/core/models/ContentFragmentData.java
+++ e-cart-app/core/src/main/java/com/adobe/aem/ecart/core/models/ContentFragmentData.java
@@ -1,1 +1,17 @@
+package com.mysite.core.models;
 
+import java.util.Map;
+
+public class ContentFragmentData{
+	
+	private Map<String, String> data;
+
+	public Map<String, String> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, String> data) {
+		this.data = data;
+	}
+	
+}
